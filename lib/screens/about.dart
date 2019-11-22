357f36a54de4a3eaa35233294eae26341e269914
randomlylelo <lzhang.0693@gmail.com>
2019-11-22 17:09:49 -0500

    about worked on a bit.. finishing later.

--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -6,15 +6,36 @@ class About extends StatelessWidget {
   final String title;
 
   About(this.title);
-  
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Center(
-        child: Text('Placeholder'),
+      body: ListView(
+        children: <Widget>[
+          Container(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.fromLTRB(16, 6, 16, 6),
+                  color: Colors.white,
+                  child: Center(
+                    child: Text(
+                      'What is FBLA?',
+                      style: DefaultTextStyle.of(context).style.apply(
+                            color: Colors.black,
+                            fontSizeFactor: 1.0,
+                            decoration: TextDecoration.none,
+                          ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
