6106a7f28cec2b7d4547b578564e2bd7e30105e2
randomlylelo <lzhang.0693@gmail.com>
2019-11-22 18:32:03 -0500

    Started working on attendance, need speed dial.

--- lib/screens/attendence.dart
+++ lib/screens/attendence.dart
@@ -2,20 +2,46 @@ import 'package:flutter/material.dart';
 
 // track chapter meeting attendence
 
-class Attendence extends StatelessWidget {
+class Attendence extends StatefulWidget {
   final String title;
 
   Attendence(this.title);
-  
+
+  @override
+  _AttendenceState createState() => _AttendenceState();
+}
+
+class _AttendenceState extends State<Attendence> {
+  // Students
+  List<String> _students = [
+    'John Doe',
+    'Jane Doe',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
-      body: Center(
-        child: Text('Placeholder'),
+      body: Container(
+        child: ListView.builder(
+          itemCount: _students.length,
+          itemBuilder: (BuildContext context, int i) {
+            return ListTile(
+              title: Text(_students[i]),
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          
+        },
+        tooltip: 'Add Members/Meeting Dates',
+        child: Icon(Icons.add),
       ),
     );
   }
 }
+
