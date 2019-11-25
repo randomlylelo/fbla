b8e9cde0891a8c34f4609a969fb398a23f527980
randomlylelo <lzhang.0693@gmail.com>
2019-11-24 21:44:26 -0500

    calendar started; just basic for now.

--- lib/screens/calendar.dart
+++ lib/screens/calendar.dart
@@ -1,25 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 // Calendar & Links to FBLA website
 // Competitive Events & Current Events
 
 // Contain Sign-Up to a current event,
-// form for either competitive event, 
+// form for either competitive event,
 // fundraiser, or community service.
 
-class Calendar extends StatelessWidget {
+class Calendar extends StatefulWidget {
   final String title;
 
   Calendar(this.title);
 
   @override
+  _CalendarState createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  CalendarController _calendarController;
+
+  @override
+  void initState() {
+    super.initState();
+    _calendarController = CalendarController();
+  }
+
+  @override
+  void dispose() {
+    _calendarController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return new Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
-      body: Center(
-        child: Text('Placeholder'),
+      body: TableCalendar(
+        calendarController: _calendarController,
       ),
     );
   }
