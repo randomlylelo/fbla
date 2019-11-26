73fa3900b65f56dd249ceadee88306cb3cb50e37
randomlylelo <lzhang.0693@gmail.com>
2019-11-26 17:39:13 -0500

    Questions added Q&A

--- lib/screens/questions.dart
+++ lib/screens/questions.dart
@@ -24,8 +24,10 @@ class _QuestionState extends State<Question> {
       child: ExpansionTile(
         title: Text(q),
         children: <Widget>[
-          Text(a),
-          Text(''),
+          Container(
+            padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
+            child: Text(a),
+          ),
         ],
       ),
     );
@@ -40,18 +42,18 @@ class _QuestionState extends State<Question> {
       body: ListView(
         children: <Widget>[
           Container(
-            padding: EdgeInsets.all(32.0),
+            padding: EdgeInsets.all(16.0),
             child: Center(
               child: Column(
                 children: <Widget>[
-                  questions('QUESTION', 'ANSWER'),
+                  questions('Why should I join FBLA?',
+                      'FBLA suits your needs as a businessperson in practice. Whether it be entrepreneurial,finance related or something similar, you will be able to practice your formality, speech, and learn more about the necessities to become successful in your future.'),
                   Divider(),
-                  questions('QUESTION', 'ANSWER'),
+                  questions('How can I join FBLA?', 'Students have the chance to join FBLA during the first two months of each school year. During this time, there will be forms passed out at the annual club fair. If you are not able to make it to this event, you may pass by the advisor\'s classroom to pick up your form.'),
                   Divider(),
-                  questions('QUESTION', 'ANSWER'),
+                  questions('What do I do in FBLA?', 'You will be assigned to a project of your choice. This project may either be a competition, presentation, or prejudged project. If you do choose a competition, you will have packets that will allow you to study for your date assigned. If you decide to have a presentation, you will have to write a speech, and practice your presentation before your assigned date of presentation. If you decide to do a prejudged project, you will have to work on a project depending on which you are assigned to and will be have frequent checkups until your deadline date.'),
                   Divider(),
-                  questions('QUESTION', 'ANSWER'),
-                  Divider(),
+                  questions('When can I see when my project is due?', 'Please refer to our calendar page'),
                 ],
               ),
             ),
