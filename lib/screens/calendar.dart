d4f404ed7d186350bcd0bf3f36b2b62ee98de40d
randomlylelo <lzhang.0693@gmail.com>
2019-11-27 15:12:32 -0500

    Calendar finished.

--- lib/screens/calendar.dart
+++ lib/screens/calendar.dart
@@ -14,14 +14,29 @@ import 'package:fbla/screens/specific/allEvents.dart';
 // fundraiser, or community service.
 
 Map<DateTime, List> _events = {
-  DateTime(2019, 11, 4): [
-    'Monthly Meeting',
-    'Monthly',
-    'Monthly Meeting',
-  ],
-  DateTime(2020, 1, 4): ['Monthly Meeting'],
-  DateTime(2019, 4, 4): ['Monthly Meeting'],
-  DateTime(2019, 4, 22): ['Monthly Meeting'],
+  DateTime(2019, 11, 4): ['Monthly Meeting'],
+  DateTime(2019, 11, 9): ['State Fall Leadership Conference'],
+  DateTime(2019, 11, 10): ['State Fall Leadership Conference'],
+  DateTime(2019, 11, 11): ['State Fall Leadership Conference'],
+  DateTime(2019, 11, 18): ['Prejudged Events Checkup'],
+  DateTime(2019, 11, 18): ['Prejudged Events Checkup'],
+  DateTime(2019, 11, 19): ['Prejudged Events Checkup'],
+  DateTime(2019, 11, 20): ['Prejudged Events Checkup'],
+  DateTime(2019, 11, 21): ['Prejudged Events Checkup'],
+  DateTime(2019, 11, 22): ['Prejudged Events Checkup'],
+  DateTime(2019, 12, 2): ['Monthly Meeting', 'Candy Box Fundrasing'],
+  DateTime(2019, 12, 7): ['District Prejudge Events'],
+  DateTime(2020, 1, 9): ['Monthly Meeting'],
+  DateTime(2020, 2, 4): ['Monthly Meeting'],
+  // DateTime(2020, 1, 9): ['District Competitions'], // TODO: GET DATE
+  DateTime(2020, 3, 12): ['State Leadership Conference'],
+  DateTime(2020, 3, 13): ['State Leadership Conference'],
+  DateTime(2020, 3, 14): ['State Leadership Conference'],
+  DateTime(2020, 3, 15): ['State Leadership Conference'],
+  DateTime(2020, 6, 29): ['National Leadership Conference'],
+  DateTime(2020, 6, 30): ['National Leadership Conference'],
+  DateTime(2020, 7, 1): ['National Leadership Conference'],
+  DateTime(2020, 7, 2): ['National Leadership Conference'],
 };
 
 Map<String, IconData> _iconsMap = {
