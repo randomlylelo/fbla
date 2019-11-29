797df2cfc8686043a3e22bb961ae9df9212ff664
randomlylelo <lzhang.0693@gmail.com>
2019-11-29 14:05:52 -0500

    Officer Revamp

--- lib/screens/officers.dart
+++ lib/screens/officers.dart
@@ -8,40 +8,68 @@ class Officers extends StatelessWidget {
 
   Officers(this.title);
 
-  Container _items(
-      BuildContext context, String name, String position, String image) {
+  Widget _officerCard(
+      BuildContext context, String name, String position, String grade) {
     return Container(
-      width: MediaQuery.of(context).size.width * 0.9,
+      padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
+      alignment: Alignment.topLeft,
       child: Card(
-        elevation: 10,
-        child: Wrap(
-          children: <Widget>[
-            Container(
-              height: MediaQuery.of(context).size.height * 0.70,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                image: DecorationImage(
-                  colorFilter: ColorFilter.mode(
-                      Colors.black.withOpacity(1), BlendMode.dstATop),
-                  image: AssetImage('./lib/assets/images/image.jpg'),
-                  fit: BoxFit.cover,
+        elevation: 2.5,
+        color: Color.fromRGBO(0, 173, 230, 1),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+          child: Row(
+            children: <Widget>[
+              Container(
+                alignment: Alignment.topLeft,
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  child: Text(
+                    name,
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w800,
+                      color: Color.fromRGBO(0, 30, 96, 1),
+                    ),
+                  ),
                 ),
               ),
-              //child: Image.network(image, fit: BoxFit.cover),
-            ),
-            Container(
-              height: MediaQuery.of(context).size.height * 0.30,
-              width: MediaQuery.of(context).size.width,
-              child: ListTile(
-                title: Text(name),
-                subtitle: Text(position),
+              Container(
+                alignment: Alignment.topLeft,
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.3,
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          position,
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromRGBO(0, 51, 160, 1),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          grade,
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromRGBO(191, 13, 62, 1),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              decoration: BoxDecoration(
-                color: Colors.white,
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -53,31 +81,60 @@ class Officers extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
         actions: <Widget>[
-          Help('This page shows Mater Lakes Academy\'s local FBLA Officer Team, on each of the cards, it shows a picture with their name and position.', 'In order to use this page, all you have to do is swipe left.')
+          Help(
+              'This page shows Mater Lakes Academy\'s local FBLA Officer Team, on each of the cards, it shows their name, position, and grade level.',
+              'In order to use this page, all you have to do is scroll down.')
         ],
       ),
-      body: Center(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          child: ListView(
-            scrollDirection: Axis.horizontal,
+      body: ListView(
+        children: <Widget>[
+          Column(
             children: <Widget>[
-              _items(context, 'Lo', 'Developer',
-                  'https://images.unsplash.com/photo-1503875154399-95d2b151e3b0?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'),
-              _items(context, 'Lo', 'Developer',
-                  'https://images.unsplash.com/photo-1503875154399-95d2b151e3b0?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'),
-              _items(context, 'Lo', 'Developer',
-                  'https://images.unsplash.com/photo-1503875154399-95d2b151e3b0?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'),
-              _items(context, 'Lo', 'Developer',
-                  'https://images.unsplash.com/photo-1503875154399-95d2b151e3b0?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'),
+              Container(
+                height: MediaQuery.of(context).size.height * .6,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('./lib/assets/images/officers.jpg'),
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.fromLTRB(10, 0, 10, 5),
+                child: Text(
+                    'Mater Lakes Academy\'s FBLA Chapter Officers & Advisor.'),
+              ),
+              Divider(),
+              Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.fromLTRB(10, 0, 10, 5),
+                child: Text(
+                  'The officers & advisors named with their position from top left to bottom right:',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              ),
+              _officerCard(context, 'Gabriel Ramirez', 'District Project Manager', '11th Grade'),
+              _officerCard(context, 'Adrian Canepa', 'Treasurer', '12th Grade'),
+              _officerCard(context, 'Paola Aleman', 'Junior Treasurer', '11th Grade'),
+              _officerCard(context, 'Vijay Rathore', 'Vice President', '12th Grade'),
+              _officerCard(context, 'Isaiah Gomez', 'Fundraising Chair', '12th Grade'),
+              _officerCard(context, 'Joshua Villamizar', 'Junior Secretary', '11th Grade'),
+              _officerCard(context, 'Isabella Milanes', 'District 25 Vice President', '12th Grade'),
+              _officerCard(context, 'Mr. Giovanni Lorenzo', 'Advisor', ''),
+              _officerCard(context, 'Anny Licor', 'President', '12th Grade'),
+              _officerCard(context, 'Melany Diaz', 'Secretary', '12th Grade'),
+              _officerCard(context, 'Nicole Mateus', 'Junior Competition Chair', '11th Grade'),
+              _officerCard(context, 'Sabrina Arcila', 'District Project Manager', '11th Grade'),
+              _officerCard(context, 'Sofia Aranda', 'District Project Manager', '11th Grade'),
+              _officerCard(context, 'Alec Castro', 'Parliamentarian', '12th Grade'),
+              _officerCard(context, 'Mya Polanco', 'Historian', '12th Grade'),
+              _officerCard(context, 'Kathryn Kalouf', 'Competition Chair', '12th Grade'),
+              _officerCard(context, 'Rosa Aleman', 'Public Relations', '12th Grade'),
             ],
           ),
-          alignment: Alignment.centerLeft,
-          // BACKGROUND COLOR TO BOX
-          decoration: BoxDecoration(
-            color: Colors.white,
-          ),
-        ),
+        ],
       ),
     );
   }
