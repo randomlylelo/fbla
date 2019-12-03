e144b6f630a9d11538e7f9ef1120e55cb887c4b9
randomlylelo <lzhang.0693@gmail.com>
2019-12-03 16:31:33 +0000

    Reset... Removed db from global, & db

--- lib/widgets/global.dart
+++ lib/widgets/global.dart
@@ -1,8 +1,5 @@
 library fbla.globals;
 
-import 'dart:io';
-import 'package:path_provider/path_provider.dart';
-
 String name = 'Student';
 String email = '';
 String password = '';
@@ -38,15 +35,4 @@ bool isAccount(String email, String password) {
   }
 
   return false;
-}
-
-// Get the directory of file.
-Future<String> get _localPath async {
-  final dir = await getApplicationDocumentsDirectory();
-  return dir.path;
-}
-
-Future<File> get _localFile async {
-  final path = await _localPath;
-  return File('$path/counter.txt');
 }
