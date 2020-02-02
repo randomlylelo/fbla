ba424fcabd7889fe1943ede000a50e0b166239bb
randomlylelo <lzhang.0693@gmail.com>
2020-02-01 22:19:12 -0500

    fixed all events

--- lib/db/eventsDB.dart
+++ lib/db/eventsDB.dart
@@ -30,10 +30,14 @@ class EventDB {
     List list2 = [];
     dynamic dummmy =
         await eventCollection.document(date.toString().split(' ')[0]).get();
+        try{
     for (int i = 0; i < dummmy['events'].length; i++) {
       if (dummmy['events'][i] != event) {
         list2.add(dummmy['events'][i]);
       }
+    }}
+    catch(e) {
+      print(e);
     }
     return list2;
   }
